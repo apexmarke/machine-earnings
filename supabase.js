10b63e4c394739e254a42a82779645c7ab3cb3c6
apexmarke <moohccaptain@gmail.com>
2026-08-20 11:16:35 +0300

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,12 @@
+const SUPABASE_URL =
+    "https://lysjkrlkwkytirojtrfb.supabase.co/rest/v1/";
+
+const SUPABASE_PUBLISHABLE_KEY =
+    "sb_publishable_1H0E0NDaa6YnNoGtg7IJNw_OL4MfEyC";
+
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
